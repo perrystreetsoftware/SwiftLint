70ead5ca6f6156406770aa048a8d40b30c3f447a
Eric Silverberg <eric@perrystreet.com>
2023-11-30 15:26:07 -0800

    Additional built-in rules

---
+++ Source/SwiftLintBuiltInRules/Rules/Lint/ObservedViewModelsRule.swift
@@ -0,0 +1,162 @@
+import SwiftSyntax
+
+@SwiftSyntaxRule
+struct ObservedViewModelsRule: Rule {
+    var configuration = SeverityConfiguration<Self>(.error)
+    
+    init() {}
+
+    let message: String = "You must use properties of an @Observed viewModel"
+
+    static let description = RuleDescription(
+        identifier: "observed_view_models",
+        name: "ObservedViewModels",
+        description: "You must use properties of an @Observed viewModel",
+        kind: .style,
+        nonTriggeringExamples: ObservedViewModelsRuleExamples.nonTriggeringExamples,
+        triggeringExamples: ObservedViewModelsRuleExamples.triggeringExamples
+    )
+}
+
+private extension ObservedViewModelsRule {
+    final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
+        var observedViewModels: [String] = []
+        var observed = false
+        var viewModelCallCount = 0
+        
+        // If we see @ObservedObject, we trigger this rule
+        static let ObservedObjectKeyword = "ObservedObject"
+        // If we see variables that end in this keyword, we trigger this rule
+        static let ProbablyAViewModelKeyword = "viewModel"
+        
+        override func visit(_ node: MemberBlockItemSyntax) -> SyntaxVisitorContinueKind {
+            if let decl = node.as(MemberBlockItemSyntax.self)?.decl.as(VariableDeclSyntax.self) {
+                if decl.attributes.first?.as(AttributeSyntax.self)?.attributeName.as(IdentifierTypeSyntax.self)?.name.text == Self.ObservedObjectKeyword {
+                    observed = true
+                }
+                
+                if let varIdentifier = decl.bindings.as(PatternBindingListSyntax.self)?.first?.pattern.as(IdentifierPatternSyntax.self)?.identifier.text {
+                    
+                    if varIdentifier.uppercased().hasSuffix(Self.ProbablyAViewModelKeyword.uppercased()) {
+                        observedViewModels.append(varIdentifier)
+                    }
+                }
+            }
+            
+            
+            if isMatching {
+                return .visitChildren
+            } else {
+                return .skipChildren
+            }
+        }
+        
+        private var isMatching: Bool {
+            observedViewModels.isNotEmpty && observed
+        }
+    
+        override func visitPost(_ node: SourceFileSyntax) {
+            if viewModelCallCount == 0 && isMatching {
+                violations.append(node.positionAfterSkippingLeadingTrivia)
+            }
+        }
+        
+        override func visitPost(_ node: MemberAccessExprSyntax) {
+            if let node = node.as(MemberAccessExprSyntax.self) {
+                if let identifierText = node.base?.as(DeclReferenceExprSyntax.self)?.baseName.text {
+                    if observedViewModels.contains(identifierText.replacingOccurrences(of: "$", with: "")) &&
+                        node.period.tokenKind == TokenKind.period {
+                        viewModelCallCount += 1
+                    }
+                }
+            }
+        }
+    }
+}
+
+internal struct ObservedViewModelsRuleExamples {
+    static let nonTriggeringExamples: [Example] = [
+        Example("""
+            struct SomeView: View {
+                @ObservedObject var viewModel: ViewModel
+            
+                var body: some View {
+                    Screen(viewModel.state)
+                }
+            
+            }
+        """),
+        Example("""
+            struct SomeView: View {
+                @ObservedObject var viewModel: ViewModel
+                var otherObject: OtherViewModel
+
+                var body: some View {
+                    Text(viewModel.title)
+                }
+            }
+        """),
+        Example("""
+            struct SomeView: View {
+                @ObservedObject var viewModel: ViewModel
+                var otherObject: OtherViewModel
+
+                var body: some View {
+                    Text(viewModel.title)
+                }
+            }
+        """),
+        Example("""
+            struct SomeView: View {
+                @StateObject var viewModel: ViewModel
+            
+                var body: some View {
+                    EmptyView()
+                }
+            
+            }
+        """),
+        Example("""
+            struct SomeView: View {
+                @ObservedObject var viewModel: ViewModel
+
+                var body: some View {
+                    Toggle("Is online", isOn: $viewModel.isOnline)
+                }
+            }
+        """),
+    ]
+
+    static var triggeringExamples: [Example] {
+        [
+            Example("""
+                struct SomeView: View {
+                    @ObservedObject var viewModel: ViewModel
+
+                    var body: some View {
+                        EmptyView()
+                    }
+                }
+            """),
+            Example("""
+                struct SomeView: View {
+                    @ObservedObject var viewModel: ViewModel
+                    var otherObject: OtherViewModel
+
+                    var body: some View {
+                        Text(otherObject.title)
+                    }
+                }
+            """),
+            Example("""
+                struct SomeView: View {
+                    @ObservedObject var someKindOfViewModel: ViewModel
+
+                    var body: some View {
+                        EmptyView()
+                    }
+                }
+            """)
+        ]
+    }
+}
---
+++ Source/SwiftLintBuiltInRules/Rules/Performance/AdaptersUseRefsNotClosuresRule.swift
@@ -0,0 +1,110 @@
+import SwiftSyntax
+
+@SwiftSyntaxRule
+struct AdaptersUseRefsNotClosuresRule: Rule {
+    var configuration = SeverityConfiguration<Self>(.error)
+
+    static let description = RuleDescription(
+        identifier: "adapters_use_refs_not_closures",
+        name: "Adapters use refs not closures",
+        description: "Prefer using function references so that SwiftUI can reduce update passes",
+        kind: .performance,
+        nonTriggeringExamples: [
+            Example("""
+            struct SomeAdapter {
+              let viewModel: AViewModel
+
+              var body: some View {
+                InnerThing(onEvent: { viewModel.onEvent() })
+              }
+            }
+            """),
+            Example("""
+            struct SomeView {
+              let viewModel: AViewModel
+
+              var body: some View {
+                InnerView(onEvent: { viewModel.onEvent() })
+              }
+            }
+            """),
+            Example("""
+            InboxTabsHostingView(showPaywall: { showPaywall?() })
+            """)
+        ],
+        triggeringExamples: [
+            Example("""
+            struct SomeAdapter {
+              let viewModel: AViewModel
+
+              var body: some View {
+                InnerView(onEvent: { viewModel.onEvent() })
+              }
+            }
+            """)
+        ]
+    )
+}
+
+private extension String {
+    var isSwiftUIView: Bool {
+        self.hasSuffix("View") || self.hasSuffix("Screen") || self.hasSuffix("Page")
+    }
+}
+
+private extension AdaptersUseRefsNotClosuresRule {
+    final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
+        // reverse walk up the tree
+        func isDeclaredInSwiftUiBody(_ node: Syntax?) -> Bool {
+            guard let node else { return false }
+            guard let patternListBinding = node.as(PatternBindingListSyntax.self) else {
+                return isDeclaredInSwiftUiBody(node.parent)
+            }
+
+            let isBody = patternListBinding.first?.pattern.as(IdentifierPatternSyntax.self)?.identifier.text == "body"
+            let isReturningView = patternListBinding.first?.typeAnnotation?.as(TypeAnnotationSyntax.self)?.type.as(SomeOrAnyTypeSyntax.self)?.constraint.as(IdentifierTypeSyntax.self)?.name.text == "View"
+
+            if isBody && isReturningView {
+                return isDeclaredInAdapterStruct(node.parent)
+            }
+
+            return isDeclaredInSwiftUiBody(node.parent)
+        }
+
+        // reverse walk up the tree
+        func isDeclaredInAdapterStruct(_ node: Syntax?) -> Bool {
+            guard let node else { return false }
+            guard let structDecl = node.as(StructDeclSyntax.self) else {
+                return isDeclaredInAdapterStruct(node.parent)
+            }
+
+            if structDecl.name.text.hasSuffix("Adapter") {
+                return true
+            }
+
+            return isDeclaredInAdapterStruct(node.parent)
+        }
+
+        func isCallingSwiftUiView(_ node: FunctionCallExprSyntax) -> Bool {
+            node.calledExpression.as(DeclReferenceExprSyntax.self)?.baseName.text.isSwiftUIView ?? false
+        }
+
+        override func visitPost(_ node: FunctionCallExprSyntax) {
+            if isCallingSwiftUiView(node) {
+                var closureCount = 0
+
+                guard isDeclaredInSwiftUiBody(node._syntaxNode) else { return }
+
+                node.arguments.forEach { argument in
+                    if argument.expression.as(ClosureExprSyntax.self) != nil {
+                        closureCount += 1
+                    }
+                }
+
+                if closureCount > 0 {
+                    violations.append(node.positionAfterSkippingLeadingTrivia)
+                }
+            }
+        }
+    }
+}
