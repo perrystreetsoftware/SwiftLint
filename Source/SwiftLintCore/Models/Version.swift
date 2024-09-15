59bcb13ca34adb0a128b4be525b96f854ea6650b
Eric Silverberg <eric@perrystreet.com>
2024-09-15 12:47:05 +0200

    Release 0.0.29

--- Source/SwiftLintCore/Models/Version.swift
+++ Source/SwiftLintCore/Models/Version.swift
@@ -9,7 +9,7 @@ public struct Version: VersionComparable {
     }
 
     /// The current SwiftLint version.
-    public static let current = Self(value: "0.0.22")
+    public static let current = Self(value: "0.0.29")
 
     /// Public initializer.
     ///
